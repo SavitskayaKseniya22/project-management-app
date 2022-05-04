a68071fdb1fdbba1de70d8390e617946f7197e2f
Nikita1814 <kravchenko1814@gmail.com>
2022-05-04 19:20:32 +0300

    fix: fix first test to work

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
   const linkElement = screen.getByText('Todo-App');
   expect(linkElement).toBeInTheDocument();
 });
