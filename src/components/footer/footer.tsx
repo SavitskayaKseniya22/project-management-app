c7943b766971c9d3c862283735103f2576fed856
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2022-05-07 23:00:18 +0300

    feat: add footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,43 @@
-export function PageFooter() {
-  return <h2>I am the footer</h2>;
-}
+import './footer.scss';
+import logo from '../../assets/svg/rs-school-js.svg';
+
+const PageFooter = () => {
+  return (
+    <footer className="footer" data-testid="footer">
+      <div className="footer-course-logo">
+        <a href="https://rs.school/react/" target="_blank" rel="noreferrer">
+          <img src={logo} alt="logo" width="100" />
+        </a>
+      </div>
+      <div className="footer-git-links">
+        <a
+          className="footer-git-link"
+          href="https://github.com/Nikita1814"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nikita Kravchenko
+        </a>
+        <a
+          className="footer-git-link"
+          href="https://github.com/ivp-dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Vadim Ilyinchik
+        </a>
+        <a
+          className="footer-git-link"
+          href="https://github.com/SavitskayaKseniya22"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Kseniya Savitskaya
+        </a>
+      </div>
+      <span className="footer-copyright">&#169; 2022</span>
+    </footer>
+  );
+};
+
+export default PageFooter;
