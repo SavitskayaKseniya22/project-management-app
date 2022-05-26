99279c9721bf53b94e340555ec2363eb200bfcb2
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2022-05-26 20:41:22 +0300

    feat: add spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,9 @@
+import './spinner.scss';
+
+export const Spinner = () => {
+  return (
+    <div className="active-search">
+      <span className="non-visible">Loading data</span>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 import './i18n';
+import { Spinner } from './components/spinner/spinner';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -14,7 +15,7 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Spinner />}>
           <App />
         </Suspense>
       </PersistGate>
