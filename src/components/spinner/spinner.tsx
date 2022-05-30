deef7127f69636c82ace23cfde9b81fd3fdd1e3f
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2022-05-30 15:02:13 +0300

    fix: spinner position

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -2,8 +2,10 @@ import './spinner.scss';
 
 export const Spinner = () => {
   return (
-    <div className="active-search">
-      <span className="non-visible">Loading data</span>
+    <div className="spinner-container">
+      <div className="active-search">
+        <span className="non-visible">Loading data</span>
+      </div>
     </div>
   );
 };
